8bc62381818c5a227626dcd2a4ee07fa53912b70
chovie tinkerer <chovieti@un-j1.kzn.21-school.ru>
2026-04-02 20:44:37 +0300

    README update and disable generate user from spring security

--- src/main/java/com/example/TicTacToeApplication.java
+++ src/main/java/com/example/TicTacToeApplication.java
@@ -2,8 +2,9 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class TicTacToeApplication {
 
 	public static void main(String[] args) {
